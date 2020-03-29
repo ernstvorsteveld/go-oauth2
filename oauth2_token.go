8719e13353bdc2c7c6a599be01baa4b07798f6b9
Ernst <ernst.vorsteveld@gmail.com>
2020-03-29 14:02:02 +0000

    added TokenType including tests

--- oauth2_token.go
+++ oauth2_token.go
@@ -5,14 +5,49 @@ import (
 )
 
 type TokenValueType uuid.UUID
+type UserIdType uuid.UUID
 
 type Token struct {
 	value    TokenValueType
 	validity ValiditySpecificationType
 }
 
-type Oauth2Token struct {
-	access_token  Token
-	refresh_token Token
-	client_id     ClientIdType
+type TokenType int
+
+const (
+	Access_token TokenType = iota
+	Refresh_token
+)
+
+var tokenTypes = map[string]TokenType{
+	"access_token":  Access_token,
+	"refresh_token": Refresh_token,
+}
+
+func (t TokenType) TokenTypeCode() string {
+	return [...]string{
+		"access_token",
+		"refresh_token",
+	}[t]
+}
+
+func TokenTypeValueOf(val string) TokenType {
+	tokenType := tokenTypes[val]
+	return tokenType
+}
+
+type Oauth2TokenType struct {
+	token_type TokenType
+	token      Token
+	client_id  ClientIdType
+	user_id    UserIdType
+}
+
+func NewOauth2Token(token_type string) Oauth2TokenType {
+	token := Oauth2TokenType{}
+	return token
+}
+
+type Oauth2Token interface {
+	IsValid() bool
 }
